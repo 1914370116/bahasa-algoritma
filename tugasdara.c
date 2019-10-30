f692b2a9353a0ef1244ca93449c3c580892e4347
Daratika Pratiwi <55613647+1914370116@users.noreply.github.com>
2019-10-30 14:44:55 +0700

    Add files via upload

---
+++ tugasdara.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Aritmatika\n");
+    printf("Nama : Daratika Pratiwi\n");
+    printf("Kelas : Reguler 1D\n");
+    printf("Npm : 1914370116\n");
+
+  int a, b, c, d, e, f;
+
+  a = 12 + 3;
+  b = 2 - 8;
+  c = 2 * 3;
+  d = 10 / 3;
+  e = 32 % 3;
+  f = ( ( 10 +3 ) -7) * 4;
+
+  printf("Isi variabel a: %d \n",a);
+  printf("Isi variabel b: %d \n",b);
+  printf("Isi variabel c: %d \n",c);
+  printf("Isi variabel d: %d \n",d);
+  printf("Isi variabel e: %d \n",e);
+  printf("Isi variabel f: %d \n",f);
+
+  return 0;
+}
